cb8da71b6ff209178be04102204795eeadcbcfee
softech.series@gmail.com <softech.series@gmail.com>
2022-03-09 14:38:50 -0500

    ok

--- Login/login.js
+++ Login/login.js
@@ -2,7 +2,7 @@
         function go(){
         
             if (document.form.password.value=='CONTRASEÑA' && document.form.login.value=='USUARIO'){ 
-                window.location="file:///C:/Users/andre/OneDrive/Escritorio/Desarrollo%20de%20Software/Introduccion%20al%20desarrollo%20de%20software/Html/Paginas%20Proyecto%20Integrador/Boletos/boletos.html" 
+                window.location="file:///C:/Users/andre/OneDrive/Escritorio/Paginas%20Proyecto%20Integrador/Boletos/boletos.html" 
                 } 
                 else{ 
                      alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
