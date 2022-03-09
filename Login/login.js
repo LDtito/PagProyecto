5f66ec77031dccc26d6a157298cfc0940c955967
softech.series@gmail.com <softech.series@gmail.com>
2022-03-09 12:10:22 -0500

    Listo

---
+++ Login/login.js
@@ -0,0 +1,10 @@
+ 
+        function go(){
+        
+            if (document.form.password.value=='CONTRASEÑA' && document.form.login.value=='USUARIO'){ 
+                window.location="file:///C:/Users/andre/OneDrive/Escritorio/Desarrollo%20de%20Software/Introduccion%20al%20desarrollo%20de%20software/Html/Paginas%20Proyecto%20Integrador/Boletos/boletos.html" 
+                } 
+                else{ 
+                     alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+                } 
+            } 
